fceb70122eefddb804d70e4bc48c81175a674019
Vitor Almeida <vitorjurunas@gmail.com>
2020-05-05 21:43:16 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+    next();
+});
+
+app.get('/', (req,res,next) =>{
+    res.send("<h1>Welcome to my API.</h1>");
+});
+
+app.get('*', (req,res,next) => {
+    res.send("<h1>Not Found</h1>")
+});
+
+app.listen(1234);
